0dc00959c40d28ebe87ff383694ff678d7d5c246
enix403 <fog.code000@gmail.com>
2023-05-31 16:17:34 +0500

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,158 @@
+import vecBlocks from "./assets/vector-blocks.png";
+import whaleLogo from "./assets/whale-logo.png";
+import QrCpde from "/Qrcode.svg";
+import Lock from "/lock.svg";
+
+import {
+  ArrowRight,
+  AtRate,
+  Check,
+  OrgApple,
+  OrgFacebook,
+  OrgGoogle,
+  Password,
+  RefCode
+} from "./icons";
+
+function Navbar() {
+  return (
+    <div className='flex h-24 items-center bg-a-dark-300 px-28'>
+      <img className='relative bottom-2' src={whaleLogo} />
+      <img className='ml-10' src={vecBlocks} />
+      <div className='ml-8'>
+        <div className='mr-6 inline'>Buy Crypto</div>
+        <div className='mr-6 inline'>Market</div>
+        <div className='mr-6 inline'>NFT</div>
+        <div className='mr-6 inline'>Promotion</div>
+        <div className='mr-6 inline'>About</div>
+        <div className='mr-6 inline'>Blog</div>
+      </div>
+      <div className='flex-1'></div>
+      <div className='rounded-xl bg-a-dark-200 px-4 py-3'>English</div>
+    </div>
+  );
+}
+
+interface InputProps {
+  placeholder: string;
+  leftIcon: JSX.Element;
+  rightIcon?: JSX.Element;
+}
+function Input(props: InputProps) {
+  return (
+    <div className='mb-6 flex h-16 rounded-2.5xl bg-a-dark-200'>
+      <p className='box-center mx-4 self-stretch'>{props.leftIcon}</p>
+      <div className='my-6 mr-5 border border-a-white-100/50'></div>
+      <input
+        className='h-full flex-1 bg-transparent'
+        placeholder={props.placeholder}
+        size={1}
+      />
+      {props.rightIcon && (
+        <>
+          <p className='box-center mr-5 self-stretch'>{props.rightIcon}</p>
+        </>
+      )}
+    </div>
+  );
+}
+
+function ProviderLogin(props: React.PropsWithChildren) {
+  return (
+    <div className='mx-3 cursor-pointer rounded-2.5xl bg-a-dark-200 hover:bg-a-white-200/10'>
+      {props.children}
+    </div>
+  );
+}
+
+function LoginPane() {
+  return (
+    <div className='h-full flex-[1] rounded-app bg-a-dark-300 px-16 py-12'>
+      <h1 className='text-3xl font-bold'>Welcome Back</h1>
+      <p className='mb-6 mt-2 w-60 text-a-white-200'>
+        Please Enter your email and password
+      </p>
+
+      <div className='mx-2'>
+        <div className='mb-10 flex h-14 w-52 cursor-pointer items-stretch rounded-2.5xl bg-a-dark-200 px-3 py-2'>
+          <div className='box-center mx-1 flex-1 rounded-1.5xl bg-a-green px-3 py-3'>
+            Login
+          </div>
+          <div className='box-center mx-1 flex-1 rounded-1.5xl px-3 py-3 transition-all hover:bg-a-green/10'>
+            SignUp
+          </div>
+        </div>
+
+        <Input
+          rightIcon={Check}
+          leftIcon={AtRate}
+          placeholder='Email Address'
+        />
+        <Input leftIcon={Password} placeholder='Password' />
+        <Input leftIcon={RefCode} placeholder='Referral Code (Optional)' />
+
+        <button className='flex w-full rounded-4xl bg-a-green p-4 shadow-white hover:bg-a-green/80'>
+          <p className='flex-1 self-center'>Continue</p>
+          <div>{ArrowRight}</div>
+        </button>
+      </div>
+
+      {/* Continue */}
+      <div className='mt-10 flex items-center px-12'>
+        <div className='flex-1 border border-a-white-100/20' />
+        <span className='mx-2 whitespace-nowrap font-bold'>
+          Or Continue with
+        </span>
+        <div className='flex-1 border border-a-white-100/20' />
+      </div>
+
+      <div className='mt-4 flex justify-center'>
+        <ProviderLogin children={OrgFacebook} />
+        <ProviderLogin children={OrgApple} />
+        <ProviderLogin children={OrgGoogle} />
+      </div>
+    </div>
+  );
+}
+
+function SecondPane() {
+  return (
+    <div className='mt-12 flex flex-[1.5] flex-col items-center justify-center lg:mt-0'>
+      <img src={QrCpde} />
+      <p className='mt-5 text-2xl font-bold'>Login With QR code</p>
+      <p className='mt-1 text-sm font-medium'>
+        Scan QR code with <span className='font-bold text-a-green'>app</span>
+      </p>
+
+      <div className='mt-8 flex items-center rounded-2.5xl bg-a-dark-300'>
+        <div className='m-1 mr-3  rounded-2.5xl bg-a-green px-3 py-2'>
+          <img src={Lock} />
+        </div>
+        <p className='mr-10 text-sm'>
+          <span className='text-a-green'>https:</span>//www.cryptowhale.com
+        </p>
+      </div>
+      <p className='mt-16 w-72 text-center text-a-white-200'>
+        Whale.io, Trade anything anywhere with Whale.io!
+      </p>
+    </div>
+  );
+}
+
+function LoginBox() {
+  return (
+    <div className='mx-auto mb-20 mt-20 w-4/5 items-stretch rounded-app bg-a-dark-900 px-4 py-6 lg:flex lg:h-[50rem] lg:w-[66rem]'>
+      <LoginPane />
+      <SecondPane />
+    </div>
+  );
+}
+
+export function App() {
+  return (
+    <>
+      <Navbar />
+      <LoginBox />
+    </>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+/* eslint-disable-next-line */
+const defaultConfig = require("tailwindcss/defaultConfig");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    fontFamily: {
+      sans: ["Poppins", ...defaultConfig.theme.fontFamily.sans]
+    },
+    extend: {
+      colors: {
+        'a-dark': {
+          100: "#161E28",
+          200: "#161D26",
+          300: "#12181F",
+          //
+          900: "#0B0E13",
+        },
+        'a-green': "#0CAF60",
+        'a-white': {
+          100: '#808195',
+          200: '#C3CDDB',
+          300: '#C4C4C4',
+          900: '#FFFFFF'
+        }
+      },
+      borderRadius: {
+        '1.5xl': '14px',
+        '2.5xl': '20px',
+        '4xl': '25px',
+        'app': '2.313rem',
+      }
+    }
+  },
+  plugins: []
+};
